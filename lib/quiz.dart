0f9ab09c803affd452fd56142816345bffbd3738
Elijah Ewers <ewerselijah@gmail.com>
2025-03-04 12:17:20 -0500

    Added some touch ups, put results screen  and summary widgets, and trying to fix flutter

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,7 +13,19 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   Widget? activeScreen;
+
+  void chooseAnswer(String answer){
+    selectedAnswers.add(answer);
+    if(selectedAnswers.length == questions.length){
+        setState((){
+          selectedAnswers = [];
+          activeScreen = StartScreen(switchScreen);
+        });
+    }
+  }
+
   @override
   void initState() {
     activeScreen = StartScreen(switchScreen);
@@ -21,7 +34,9 @@ class _QuizState extends State<Quiz> {
 
   void switchScreen() {
     setState(() {
-      activeScreen = QuestionsScreen();
+      activeScreen = QuestionsScreen(
+        onSelectedAnswer: chooseAnswer,
+        );
     });
   }
 
