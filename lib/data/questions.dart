ae55389615f3f37b98daeb357b7e6e8ae36f89a8
Elijah Ewers <ewerselijah@gmail.com>
2025-03-24 00:13:18 -0400

    Added one more queestion to data page

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -8,16 +8,16 @@ var questions = [
   QuizQuestion(question[2],choices[2]),
   QuizQuestion(question[3],choices[3]),
   QuizQuestion(question[4],choices[4]),
- 
+  QuizQuestion(question[5],choices[5])
 ];
 
-List<String> question  = 
-[
-'Who was the first emperor of Rome?',
-"The Great Pyramid of Giza was built during which civilization?",
-"What famous city was buried under volcanic ash in 79 AD?",
-"What was the writing system of the ancient Sumerians called?",
-"Which famous conqueror created one of the largest empires in history before dying at 32?"
+List<String> question = [
+  'Who was the first emperor of Rome?',
+  'The Great Pyramid of Giza was built during which civilization?',
+  'What famous city was buried under volcanic ash in 79 AD?',
+  'What was the writing system of the ancient Sumerians called?',
+  'Which famous conqueror created one of the largest empires in history before dying at 32?',
+  'Which war was fought between the North and South regions of the United States from 1861 to 1865?'
 ];
 
 List<List<String>> choices = [
@@ -25,13 +25,15 @@ List<List<String>> choices = [
   ["Greek", "Roman", "Egyptian", "Mesopotamian"],
   ["Pompeii", "Carthage", "Athens", "Troy"],
   ["Hieroglyphics", "Cuneiform", "Sanskrit", "Latin"],
-  ["Alexander the Great", "Genghis Khan", "Saddam Hussein", "Napoleon Bonaparte"]
+  ["Alexander the Great", "Genghis Khan", "Saddam Hussein", "Napoleon Bonaparte"],
+  ["American Revolution", "War of 1812", "American Civil War", "Spanish-American War"]
 ];
 
 List<String> correctAnswers = [
-  "Augustus",  
-  "Egyptian",  
-  "Pompeii",  
-  "Cuneiform",  
-  "Alexander the Great"
+  "Augustus",
+  "Egyptian",
+  "Pompeii",
+  "Cuneiform",
+  "Alexander the Great",
+  "American Civil War"
 ];
